14c039c3ee0691b5da2b19906e08e83c941f4b0a
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2025-08-21 22:09:18 +0300

    add page3-5
    Took 3 minutes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Page3 from '@/views/Page3.vue'
+import Page4 from '@/views/Page4.vue'
+import Page5 from '@/views/Page5.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +20,9 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    { path: './page3', name: 'page3', component: Page3, },
+    { path: './page4', name: 'page4', component: Page4, },
+    { path: './page5', name: 'page5', component: Page5, },
   ],
 })
 
---
+++ src/views/Page5.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Page 5</h1>
+  </main>
+</template>
